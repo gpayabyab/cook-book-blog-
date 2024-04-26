090749a2bafa7a1183157ac51809cd34cfd5c8b7
BaharMahmud <bahar.a.mahmud@gmail.com>
2024-04-25 21:18:31 -0400

    Added App.css, app.jsx,package.json,Overall client folder is all done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'; // Import if using Redux
+import { BrowserRouter } from 'react-router-dom';
+import App from './App'; // Adjust the path as necessary
+import store from './utils/store'; // Adjust the path as necessary if using Redux
+import './index.css'; // Assuming global styles are placed here
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}> {/* Only include this line if using Redux */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
---
+++ client/src/utils/actions/authActions.js
@@ -0,0 +1,14 @@
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGOUT = 'LOGOUT';
+// Action Creators
+export const loginSuccess = (userData) => {
+    return {
+        type: LOGIN_SUCCESS,
+        payload: userData
+    };
+};
+export const logout = () => {
+    return {
+        type: LOGOUT
+    };
+};
