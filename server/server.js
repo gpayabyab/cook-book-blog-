cd1bbc35ac26a5fa4ca18212fee95d73b151a999
KittiyaLooknam <kittiya.goldberg1@gmail.com>
2024-04-29 13:39:29 -0400

    add authReducer.js and update server.js

--- server/server.js
+++ server/server.js
@@ -13,6 +13,33 @@ const server = new ApolloServer({
   resolvers,
 });
 //complete code below
-const startApolloServer = {};
+// const startApolloServer = {};
+
+//startApolloServer();
+
+
+//  Not sure if this is needed
+// Apply Apollo GraphQL middleware to Express
+server.applyMiddleware({ app });
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const startApolloServer = async () => {
+  await db.connectDb(); // Connect to the database
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+    console.log(`GraphQL Playground available at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+};
 
 startApolloServer();
+
+module.exports = { app, server };
