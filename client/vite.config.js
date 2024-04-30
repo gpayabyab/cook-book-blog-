f27d280cb72eb1f8099cae2e3690487f373e8670
KittiyaLooknam <kittiya.goldberg1@gmail.com>
2024-04-29 21:53:49 -0400

    edite the json and vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000 // You can specify the port you want
+    port: 3000, // You can specify the port you want
+    open: true, // Opens the browser after server is started
+    proxy: {
+      // Proxying websockets or socket.io 
+      '/graphql': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+      }, 
+    }
   }
 });
+
