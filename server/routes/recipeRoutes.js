78e55dccddcc0529c9b447ec25e18e6a9fad3e16
Gabrielle 🌿 <gabrielle@gabrieleslaptop.mynetworksettings.com>
2024-05-01 20:39:33 -0400

    fixed error in resolvers file and removed some content in the recipeRoutes.js file

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -11,10 +11,6 @@ const router = express.Router();
 router.get('/', getAllRecipes);
 // Route to get a single recipe by id
 router.get('/:id', getRecipe);
-// Route to create a new recipe
-router.post('/', createRecipe);
-// Route to update a recipe by id
-router.put('/:id', updateRecipe);
 // Route to delete a recipe by id
 router.delete('/:id', deleteRecipe);
 module.exports = router;
