7ec28e9659513ad6a58e2fdbcd6a5943f0f95209
KittiyaLooknam <kittiya.goldberg1@gmail.com>
2024-05-01 21:58:10 -0400

    edited the data seeds

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,12 +1,12 @@
 const db = require('../config/connection');
 const { Recipe } = require('../models');
-const { recipesSeeds } = require('./recipesSeeds.json');
+const  recipesSeeds  = require('./recipesSeeds.json');
 const cleanDB = require('./cleanDB');
 
 db.once('open', async () => {
-  //await cleanDB('Recipe', 'recipe');
+ await cleanDB('Recipe', 'recipes');
 
-  await Recipe.create(recipesSeeds);
+  await Recipe.insertMany(recipesSeeds);
 
   console.log('all done!');
   process.exit(0);
