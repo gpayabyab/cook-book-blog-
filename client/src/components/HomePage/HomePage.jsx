793547a9febd6657d581c0599a203926c335dcbf
BaharMahmud <bahar.a.mahmud@gmail.com>
2024-05-06 16:57:45 -0400

    last changes

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -4,7 +4,7 @@ import './HomePage.css';
 const Home = () => {
     return (
         <div className="home-container">
-            <h1 style={{ fontSize: '150px', textJustify: 'left' }}>Welcome 👩🏻‍🍳 </h1>
+            <h1 ><span style={{ fontSize: '150px' }}>W</span>elcome 👩🏻‍🍳 </h1>
             <h1> to Our Recipe Blog!</h1>
             <p>Explore a world of delicious recipes and culinary adventures. Sign up or log in to save your favorite recipes and more!</p>
             <p>Discover new dishes, share your own creations, and get inspired by the rich flavors from around the world.</p>
--- client/src/components/RecipeList/RecipeList.jsx
+++ client/src/components/RecipeList/RecipeList.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import { useQuery, useMutation } from "@apollo/client";
-import { GET_ALL_RECIPES,GET_USER } from "../../utils/queries";
+import { GET_ALL_RECIPES, GET_USER } from "../../utils/queries";
 import { SAVE_RECIPE } from "../../utils/mutation";
 import { Link } from "react-router-dom";
 import Auth from "../../utils/auth";
+import { MdOutlineSaveAlt } from "react-icons/md";
+import { FaCheck } from "react-icons/fa";
 
 import { saveRecipeIds, getSavedRecipeIds } from "../../utils/localStorage";
 
@@ -13,31 +15,28 @@ function RecipeList() {
   const { data, loading, error } = useQuery(GET_ALL_RECIPES, {
     pollInterval: 100,
   });
-   const user = useQuery(GET_USER)
+  const user = useQuery(GET_USER);
 
-   const userData = user.data?.user || {}
+  const userData = user.data?.user || {};
 
-  
   const recipeData = data?.recipes || [];
 
-  
   const [savedRecipeIds, setSavedRecipeIds] = useState(getSavedRecipeIds());
   const [saveRecipeToUser] = useMutation(SAVE_RECIPE);
-  
+
   useEffect(() => {
     return () => saveRecipeIds(savedRecipeIds);
   });
 
-  useEffect(()=> {
-    const userSavedRecipes = userData?.savedRecipes.map((item)=> item._id)
+  useEffect(() => {
+    const userSavedRecipes = userData?.savedRecipes.map((item) => item._id);
     setSavedRecipeIds(userSavedRecipes);
-  },[])
- 
+  }, []);
 
   const handleSave = async (recipeId) => {
     try {
       const { data } = await saveRecipeToUser({ variables: { recipeId } });
-      
+
       setSavedRecipeIds([...savedRecipeIds, recipeId]);
     } catch (error) {
       console.error(error);
@@ -47,19 +46,30 @@ function RecipeList() {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error</p>;
   return (
-    <div>
+    <div className="flex">
       {recipeData.map(({ _id, name, ingredients, description, image }) => (
-        <div className='recipe-container' key={_id}>
+        <div className="recipe-container" key={_id} style={{ marginBottom: '2%'}}>
           <h3>{name}</h3>
-          <p style={{color:'purple'}}>{Array.isArray(ingredients)? ingredients.join(', '): ''}</p>
-          <p>{description}</p>
-          <img src={image} alt={name} width={100}/>
-          <button className='saveButton'
-          type="button"
-          disabled={savedRecipeIds?.some((savedId)=> savedId === _id)}
-          onClick={() => handleSave(_id)}
-          >
-            {savedRecipeIds?.some((savedId)=> savedId === _id) ? 'saved' : 'save'}
+          <img src={image} alt={name} width={200} height={250} style={{borderRadius: '30px'}} />
+          <div >
+            <p><strong>Ingredients:</strong> </p>
+            <ul style={{ color: "purple", display: 'flex', flexDirection: 'column', flexWrap:'wrap', height: '200px', width: '150px' }}>
+              {/* {Array.isArray(ingredients) ? ingredients.join(", ") : ""} */}
+              {ingredients?.map((ingredient) => (
+                <li style={{marginLeft: '5px', marginRight: '25px'}}>{ingredient}</li>
+              ))}
+            </ul>
+            <p style={{padding: '20px', height: '100px'}}><strong>Instructions:</strong> {description}</p>
+          </div>
+            <button
+              className="saveButton"
+              type="button"
+              disabled={savedRecipeIds?.some((savedId) => savedId === _id)}
+              onClick={() => handleSave(_id)}
+            >
+              {savedRecipeIds?.some((savedId) => savedId === _id)
+                ? <FaCheck />
+                :<MdOutlineSaveAlt/>}
             </button>
         </div>
       ))}
